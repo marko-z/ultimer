85a330d618b3c4f5d4962b3869bececfc4d3e4bb
marko-z <m.zakrzwsk@gmail.com>
2025-09-02 17:06:37 +0200

    fixed package targets (errors with features not existing macos version)

--- Package.swift
+++ Package.swift
@@ -1,23 +1,18 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "CoreTimer",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v16),
+        .watchOS(.v6)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CoreTimer",
-            targets: ["CoreTimer"]),
+        .library(name: "CoreTimer", targets: ["CoreTimer"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CoreTimer"),
-        .testTarget(
-            name: "CoreTimerTests",
-            dependencies: ["CoreTimer"]),
+        .target(name: "CoreTimer"),
+        .testTarget(name: "CoreTimerTests", dependencies: ["CoreTimer"]),
     ]
 )
